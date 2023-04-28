99e4ca470e7bddfe4b5eaa2134eea2702542fd57
adnan007d <adnanmansuri54@gmail.com>
2023-04-28 16:55:27 +0530

    validation bitches

--- controllers/contact.go
+++ controllers/contact.go
@@ -1,26 +1,37 @@
 package controllers
 
 import (
-	"fmt"
 	"go-api/models"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
 )
 
 func CreateContact(c *gin.Context) {
 
 	var body struct {
-		Name    string
-		Email   string
-		Message string
+		Name    string `form:"name" json:"name" validate:"required"`
+		Email   string `form:"email" json:"email" validate:"required,email"`
+		Message string `form:"message" json:"message" validate:"required"`
 	}
 
 	c.Bind(&body)
+
+	validate := validator.New()
+
+	err := validate.Struct(body)
+
+	if err != nil {
+		errorMap := make(map[string]string)
+		for _, fieldErr := range err.(validator.ValidationErrors) {
+			errorMap[fieldErr.Field()] = fieldErr.Tag()
+		}
+		c.JSON(http.StatusBadRequest, errorMap)
+		return
+	}
+
 	contact := models.Contact{Name: body.Name, Email: body.Email, Message: body.Message}
 
-	fmt.Println(body)
 	c.JSON(http.StatusAccepted, contact)
-	// contact := models.Contact{Name: "Bitch"}
-	// initializers.DB.Create(&contact)
 }
